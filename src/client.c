ebaef66dac5d304b970c26d7ff2cd9d8b81bc4d0
henryallucci <151469261+henryallucci@users.noreply.github.com>
2023-11-19 22:13:41 -0300

    Update client.c

--- src/client.c
+++ src/client.c
@@ -1,5 +1,11 @@
 #include <bignumber.h>
 
 int main(){
+    int n;
+    scanf("%d",&n);
+    int tamanho = tamanho_numero(n);
+    int* vetor = malloc(tamanho*sizeof(int));
+    numero_em_vetor(n, tamanho, vetor);
+    
     return 0;
 }
