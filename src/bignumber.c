361279a699bea398d405d3e809210724cad79f3c
henryallucci <151469261+henryallucci@users.noreply.github.com>
2023-11-19 22:09:52 -0300

    Update bignumber.c

--- src/bignumber.c
+++ src/bignumber.c
@@ -1,1 +1,16 @@
 #include <bignumber.h>
+
+int tamanho_numero(int n){
+    if (n < 10){
+        return 1;
+    } else {
+        return tamanho_numero(n/10) + 1;
+    }
+}
+
+void numero_em_vetor(int n, int tamanho, int* vetor){
+    for (int i=0; i<tamanho; i++){
+        vetor[i] = n % 10;
+        n = n / 10;
+    }
+}
