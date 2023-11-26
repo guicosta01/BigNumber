815ea8467cf12333b1b267d000b33b6fe311695e
henryallucci <151469261+henryallucci@users.noreply.github.com>
2023-11-26 16:38:53 -0300

    Update bignumber.c

--- src/bignumber.c
+++ src/bignumber.c
@@ -1,7 +1,4 @@
 #include <bignumber.h>
-#include <stdio.h>
-#include <stdlib.h>
-
 int tamanho_numero(int n){
     if (n < 10){
         return 1;
@@ -41,26 +38,90 @@ int soma(int vetor[], int tamanho, int vetor2[], int tamanho2){
     }
 }
 
-void resultado(int vetor[], int tamanho, int vetor2[], int tamanho2){
-    printf("Resultado: ");
-    if(tamanho >= tamanho2){
-        if(vetor[tamanho] != 0){
-            for(int i = tamanho; i >= 0; i--){
-                printf("%d", vetor[i]);
-            }    
-        } else {
-            for(int i = tamanho-1; i >= 0; i--){
-                printf("%d", vetor[i]);
+int* maior_numero(int vetor[], int tamanho, int vetor2[], int tamanho2){
+    if(tamanho>tamanho2){
+        return vetor;
+    } else if (tamanho<tamanho2) {
+        return vetor2;
+    } else {
+        for (int i=tamanho-1; i>=0; i--){
+            if (vetor[i] > vetor2[i]){
+                return vetor;
+            } else if (vetor[i] < vetor2[i]){
+                return vetor2;
+            }
+        }
+    }    
+}
+
+void subtracao(int vetor[], int tamanho, int vetor2[], int tamanho2, int maior[]) {
+    if (maior == vetor) {
+        for (int i = 0; i < tamanho; i++) {
+            if (vetor[i] >= vetor2[i]) {
+                vetor[i] = vetor[i] - vetor2[i];
+            } else {
+                vetor[i] = 10 + vetor[i] - vetor2[i];
+                vetor[i + 1] -= 1;
             }
         }
     } else {
-        if(vetor2[tamanho2] != 0){
-            for(int i = tamanho2; i >= 0; i--){
-                printf("%d", vetor2[i]);
+        for (int i = 0; i < tamanho2; i++) {
+            if (vetor2[i] >= vetor[i]) {
+                vetor2[i] = vetor2[i] - vetor[i];
+            } else {
+                vetor2[i] = 10 + vetor2[i] - vetor[i];
+                vetor2[i + 1] -= 1;
+            }
+        }
+    }
+}
+
+void resultado(int vetor[], int tamanho, int vetor2[], int tamanho2, int maior[], char operacao){
+    printf("Resultado: ");
+    if(operacao == '+'){
+        if(tamanho >= tamanho2){
+            if(vetor[tamanho] != 0){
+                for(int i = tamanho; i >= 0; i--){
+                    printf("%d", vetor[i]);
+                }    
+            } else {
+                for(int i = tamanho-1; i >= 0; i--){
+                    printf("%d", vetor[i]);
+                }
             }
         } else {
-            for(int i = tamanho2-1; i >= 0; i--){
-                printf("%d", vetor2[i]);
+            if(vetor2[tamanho2] != 0){
+                for(int i = tamanho2; i >= 0; i--){
+                    printf("%d", vetor2[i]);
+                }
+            } else {
+                for(int i = tamanho2-1; i >= 0; i--){
+                    printf("%d", vetor2[i]);
+                }
+            }
+        }
+    } else if (operacao == '-'){
+        if(maior == vetor){
+            if(vetor[tamanho] != 0){
+                for(int i = tamanho; i >= 0; i--){
+                    printf("%d", vetor[i]);
+                }
+            } else {
+                for(int i = tamanho-1; i >= 0; i--){
+                    printf("%d", vetor[i]);
+                }
+            }
+        } else {
+            if(vetor2[tamanho2] != 0){
+                printf("-");
+                for(int i = tamanho2; i >= 0; i--){
+                    printf("%d", vetor2[i]);
+                }
+            } else {
+                printf("-");
+                for(int i = tamanho2-1; i >= 0; i--){
+                    printf("%d", vetor2[i]);
+                }
             }
         }
     }
