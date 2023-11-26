953fe6b4d502da7288ca334bf77a6299e2676144
henryallucci <151469261+henryallucci@users.noreply.github.com>
2023-11-26 14:45:36 -0300

    Update client.c

--- src/client.c
+++ src/client.c
@@ -2,10 +2,28 @@
 
 int main(){
     int n;
+    printf("Digite um número:\n");
     scanf("%d",&n);
     int tamanho = tamanho_numero(n);
     int* vetor = malloc(tamanho*sizeof(int));
     numero_em_vetor(n, tamanho, vetor);
+    
+    int n2;
+    printf("Digite um segundo número:\n");
+    scanf("%d",&n2);
+    int tamanho2 = tamanho_numero(n2);
+    int* vetor2 = malloc(tamanho2*sizeof(int));
+    numero_em_vetor(n2, tamanho2, vetor2);
+    
+    char operacao;
+    printf("Digite a operação:\n");
+    getchar();
+    scanf("%c", &operacao);
+
+    if (operacao == '+'){
+        soma(vetor, tamanho, vetor2, tamanho2);
+        resultado(vetor, tamanho, vetor2, tamanho2);
+    }
     
     return 0;
 }
