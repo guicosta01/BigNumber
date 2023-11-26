5f8295dd005f3cf02fcfc40144a1d7208122cfdb
henryallucci <151469261+henryallucci@users.noreply.github.com>
2023-11-26 16:40:12 -0300

    Update bignumber.h

--- src/bignumber.h
+++ src/bignumber.h
@@ -7,4 +7,8 @@ void numero_em_vetor(int n, int tamanho, int* vetor);
 
 int soma(int vetor[], int tamanho, int vetor2[], int tamanho2);
 
-void resultado(int vetor[], int tamanho, int vetor2[], int tamanho2);
+int* maior_numero(int vetor[], int tamanho, int vetor2[], int tamanho2);
+
+void subtracao(int vetor[], int tamanho, int vetor2[], int tamanho2, int maior[]);
+
+void resultado(int vetor[], int tamanho, int vetor2[], int tamanho2, int maior[], char operacao);
